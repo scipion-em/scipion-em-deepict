16ae57e06e392193f224fc43f1c225a00714a525
Vilax <jlvilasprieto@gmail.com>
2023-04-17 13:11:54 +0200

    output fully working

--- deepict/protocols/protocol_deepict_segmentation.py
+++ deepict/protocols/protocol_deepict_segmentation.py
@@ -369,7 +369,7 @@ class DeepictSegmentation(EMProtocol, ProtTomoBase):
         tsId = ts.getTsId()
 
         typeOfModel = os.path.split(os.path.splitext(self.getModel())[0])[1]
-        print(typeOfModel)
+
         predfolder = os.path.join('predictions', typeOfModel, tsId, 'memb')
         outputSeg = os.path.join(self._getExtraPath(tsId), predfolder)
 
@@ -380,7 +380,7 @@ class DeepictSegmentation(EMProtocol, ProtTomoBase):
                                              'post_processed_prediction.mrc'))
         newTomogram.setTsId(tsId)
         newTomogram.setSamplingRate(ts.getSamplingRate())
-        print(os.path.join(outputSeg,'post_processed_prediction.mrc'))
+
         # Set default tomogram origin
         newTomogram.setOrigin(newOrigin=None)
         newTomogram.setAcquisition(ts.getAcquisition())
